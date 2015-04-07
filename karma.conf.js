c7aed2e6d31f09d00c9f5b5a51690ec295615e1b
Jarrett Meyer <jarrettmeyer@gmail.com>
2015-04-07 15:16:29 -0400

    Adds bootstrap to karma configuration.

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function(config) {
 
         files: [
             'bower_components/jquery/dist/jquery.js',
+            'bower_components/bootstrap/dist/js/bootstrap.js',
             'bower_components/angularjs/angular.js',
             'bower_components/angular-mocks/angular-mocks.js',
             'bower_components/angular-route/angular-route.js',
